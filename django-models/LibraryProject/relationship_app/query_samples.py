c9459709e574e7cc02f12d91b25e0d6036ed8e3d
Byrone Songa <byronesonga68@gmail.com>
2025-09-07 02:04:24 -0600

    complex models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -35,7 +35,7 @@ def list_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     """Retrieve the librarian for a library."""
     try:
-        library = Library.objects.get(library=library_name)
+        library = Library.objects.get(library=library)
         librarian = library.librarian
         print(f"Librarian for {library.name}: {librarian.name}")
     except Library.DoesNotExist:
