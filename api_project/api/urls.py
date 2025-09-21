36e1518bb7c146d07f7236636ab50e7f55ad29c7
Byrone Songa <byronesonga68@gmail.com>
2025-09-21 15:12:24 +0300

    crud operations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path , include
+from .views import BookList , BookViewSet
+from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+]
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,17 @@
+from rest_framework import viewsets
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()          # fetch all Book records
     serializer_class = BookSerializer      # use BookSerializer to serialize them
 
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Book instances.
+    Provides `list`, `create`, `retrieve`, `update`, and `destroy` actions automatically.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
